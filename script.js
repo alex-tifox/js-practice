2eb39179790146831723b4191d4ff2d5cb2cf0d4
alex-tifox <tilnyi.olexander@gmail.com>
2019-09-17 10:57:39 +0200

    first js

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('Hello world');
+
+var someData = 5;
+console.log(someData);
+console.log('string'*9);
+
+//alert("Hello");
+
+// let answer = confirm('are you here');
+
+// console.log(answer);
+
+let answer = prompt('are you here', 'yes');
+
+console.log(typeof(answer));
